2b03846e9dfa6dd5df96c4e6effc80258157ef0c
phil.jambaud@gmail.com <&[GzLj2?@qP9"uVD>
2020-06-02 12:45:22 +0200

    - add WereWolf event

--- src/main/java/io/github/ph1lou/RoleExample.java
+++ src/main/java/io/github/ph1lou/RoleExample.java
@@ -2,14 +2,22 @@ package io.github.ph1lou;
 
 
 import io.github.ph1lou.pluginlgapi.GetWereWolfAPI;
+import io.github.ph1lou.pluginlgapi.PlayerWW;
 import io.github.ph1lou.pluginlgapi.WereWolfAPI;
-import io.github.ph1lou.pluginlgapi.enumlg.Camp;
+import io.github.ph1lou.pluginlgapi.enumlg.*;
 import io.github.ph1lou.pluginlgapi.events.DayEvent;
+import io.github.ph1lou.pluginlgapi.events.NewWereWolfEvent;
 import io.github.ph1lou.pluginlgapi.events.NightEvent;
+import io.github.ph1lou.pluginlgapi.events.WereWolfListEvent;
 import io.github.ph1lou.pluginlgapi.rolesattributs.Roles;
+import org.bukkit.Bukkit;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scoreboard.Team;
 
 import java.util.UUID;
 
@@ -87,8 +95,91 @@ public class RoleExample implements Roles, Listener,Cloneable {
 
     @Override
     public void recoverPotionEffect(Player player) {
+        if (game.getConfig().getScenarioValues().get(ScenarioLG.CAT_EYES)) {
+            player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION,Integer.MAX_VALUE,0,false,false));
+        }
         //à l'annonce des rôles et quand je rez
     }
+
+    @EventHandler
+    public void onWereWolfList(WereWolfListEvent event){
+
+        PlayerWW lg = game.getPlayersWW().get(uuid);
+
+        if(!isWereWolf()){
+            return;
+        }
+
+        Team team=game.getWereWolfScoreBoard().getTeam(lg.getName());
+
+        if (game.getConfig().getConfigValues().get(ToolLG.RED_NAME_TAG)) {
+            if(game.getHosts().contains(uuid)){
+                team.setPrefix(game.translate("werewolf.commands.admin.host.tag")+"§4");
+            }
+            else team.setPrefix("§4");
+        }
+        lg.setScoreBoard(game.getWereWolfScoreBoard());
+
+        if(!lg.isState(State.ALIVE)) {
+            return;
+        }
+
+        if (Bukkit.getPlayer(uuid) == null) {
+            return;
+        }
+
+        Player player = Bukkit.getPlayer(uuid);
+        player.sendMessage(game.translate("werewolf.role.werewolf.see_others"));
+        player.playSound(player.getLocation(), Sound.WOLF_HOWL, 1, 20);
+        player.setScoreboard(game.getWereWolfScoreBoard());
+    }
+
+    @EventHandler
+    public void onNewWereWolf(NewWereWolfEvent event) {
+
+        PlayerWW plg = game.getPlayersWW().get(uuid);
+
+        if(uuid.equals(event.getUuid())){
+
+            setCamp(Camp.WEREWOLF);
+
+            if (game.getConfig().getTimerValues().get(TimerLG.WEREWOLF_LIST) < 0) {
+
+                Team team=game.getWereWolfScoreBoard().getTeam(plg.getName());
+
+                if (game.getConfig().getConfigValues().get(ToolLG.RED_NAME_TAG)) {
+                    if(game.getHosts().contains(uuid)){
+                        team.setPrefix(game.translate("werewolf.commands.admin.host.tag")+"§4");
+                    }
+                    else team.setPrefix("§4");
+                }
+                plg.setScoreBoard(game.getWereWolfScoreBoard());
+            }
+
+            if(Bukkit.getPlayer(uuid)!=null) {
+                Player player = Bukkit.getPlayer(uuid);
+                player.setScoreboard(game.getWereWolfScoreBoard());
+                player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION,Integer.MAX_VALUE,0,false,false));
+                player.sendMessage(game.translate("werewolf.role.werewolf.go_to_the_werewolf_camp"));
+                player.playSound(player.getLocation(),Sound.WOLF_HOWL, 1, 20);
+                if (game.isDay(Day.NIGHT)) {
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, Integer.MAX_VALUE,-1,false,false));
+                }
+            }
+        }
+        else if(isWereWolf()){
+            if (game.getConfig().getTimerValues().get(TimerLG.WEREWOLF_LIST) < 0) {
+                if (plg.isState(State.ALIVE)) {
+                    if(Bukkit.getPlayer(uuid) != null){
+                        Player lg1 = Bukkit.getPlayer(uuid);
+                        lg1.sendMessage(game.translate("werewolf.role.werewolf.new_werewolf"));
+                        lg1.playSound(lg1.getLocation(), Sound.WOLF_HOWL, 1, 20);
+                    }
+                }
+            }
+        }
+    }
+
 
     @Override
     public boolean isWereWolf() {
