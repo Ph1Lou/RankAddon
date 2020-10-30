e5bfe8fedc446bf6c9adc8bd9071c9b5fdab1ffb
Ph1Lou <&[GzLj2?@qP9"uVD>
2020-10-30 21:36:26 +0100

    - update

--- src/main/java/io/github/ph1lou/rank/Main.java
+++ src/main/java/io/github/ph1lou/rank/Main.java
@@ -1,15 +1,36 @@
 package io.github.ph1lou.rank;
 
+import io.github.ph1lou.werewolfapi.AddonRegister;
 import io.github.ph1lou.werewolfapi.GetWereWolfAPI;
+import io.github.ph1lou.werewolfapi.RegisterManager;
+import io.github.ph1lou.werewolfapi.enumlg.Sounds;
+import io.github.ph1lou.werewolfapi.enumlg.UniversalMaterial;
+import io.github.ph1lou.werewolfapi.utils.ItemBuilder;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Arrays;
+import java.util.UUID;
 
 public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
-        GetWereWolfAPI api = (GetWereWolfAPI)Bukkit.getPluginManager().getPlugin("WereWOlfPlugin");
+        GetWereWolfAPI api = (GetWereWolfAPI)Bukkit.getPluginManager().getPlugin("WereWolfPlugin");
+        RegisterManager registerManager = api.getRegisterManager();
+        registerManager
+                .registerAddon(new AddonRegister("werewolf.rank_addon",
+                        "fr",
+                        this,
+                        new ItemBuilder(UniversalMaterial.BOOK.getType())
+                        .setDisplayName("Addon de Rang")
+                        .setLore(Arrays.asList("Customise les Prefixes","et les Suffixes"))
+                        .build()
+                )
+                .addAuthors("Ph1Lou",UUID.fromString("056be797-2a0b-4807-9af5-37faf5384396"))
+                .setAction(Sounds.ANVIL_USE::play));
+
         Bukkit.getPluginManager().registerEvents(new PrefixManager(this,api), this);
     }
 }
